4078ddf653f830c1e2c7561043e38eb128f7b94e
kakl <kakl@astro.cz>
2020-12-21 20:31:18 +0100

    automatic precipitation recording

---
+++ sw/detect-record.py
@@ -0,0 +1,107 @@
+#!/usr/bin/python3
+
+import pyaudio
+import math
+import struct
+import wave
+import time
+import os
+import sys
+
+Threshold = 200
+
+SHORT_NORMALIZE = (1.0/32768.0)
+CHUNK = 1024
+FORMAT = pyaudio.paInt16
+CHANNELS = 2
+RATE = 96000
+swidth = 2
+
+
+TIMEOUT_LENGTH = 5
+
+f_name_directory = r'.'
+
+dev_index = 6
+
+class Recorder:
+
+    @staticmethod
+    def rms(frame):
+        count = len(frame) / swidth
+        format = "%dh" % (count)
+        shorts = struct.unpack(format, frame)
+
+        sum_squares = 0.0
+        for sample in shorts:
+            n = sample * SHORT_NORMALIZE
+            sum_squares += n * n
+        rms = math.pow(sum_squares / count, 0.5)
+
+        return rms * 1000
+
+    def __init__(self):
+        self.p = pyaudio.PyAudio()
+        self.stream = self.p.open(format=FORMAT,
+                                  channels=CHANNELS,
+                                  rate=RATE,
+                                  input=True,
+                                  frames_per_buffer=CHUNK,
+                                  input_device_index = dev_index)
+
+    def record(self):
+        print('Noise detected, recording beginning')
+        rec = []
+        current = time.time()
+        end = time.time() + TIMEOUT_LENGTH
+
+        while current <= end:
+
+            data = self.stream.read(CHUNK)
+            if self.rms(data) >= Threshold: end = time.time() + TIMEOUT_LENGTH
+
+            current = time.time()
+            rec.append(data)
+        self.write(b''.join(rec))
+
+    def write(self, recording):
+        #n_files = len(os.listdir(f_name_directory))
+        n_files = str(time.time()).split('.')[0]
+        
+        filename = os.path.join(f_name_directory, '{}.wav'.format(n_files))
+
+        wf = wave.open(filename, 'wb')
+        wf.setnchannels(CHANNELS)
+        wf.setsampwidth(self.p.get_sample_size(FORMAT))
+        wf.setframerate(RATE)
+        wf.writeframes(recording)
+        wf.close()
+        print('Written to file: {}'.format(filename))
+        print('Returning to listening')
+
+
+
+    def listen(self):
+        print('Listening beginning')
+        while True:
+            input = self.stream.read(CHUNK)
+            rms_val = self.rms(input)
+            if rms_val > Threshold:
+                self.record()
+
+                
+    def close(self):
+        self.stream.close()
+                
+while (True):
+    try:
+        a = Recorder()
+        a.listen()
+    except KeyboardInterrupt:
+    	a.close()
+    	print('\nBye')
+    	sys.exit()
+    except:
+        a.close()
+        pass
+
